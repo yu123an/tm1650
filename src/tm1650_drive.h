8bd4f9cf362bca5b31e31d4858160ee6b80e6a36
yu123an <43343181+yu123an@users.noreply.github.com>
2019-11-12 14:08:06 +0800

    Update tm1650_drive.h

--- src/tm1650_drive.h
+++ src/tm1650_drive.h
@@ -1,4 +1,110 @@
 #include "stm8s.h"
-#include "tm1650_drive.cpp"
+#include "Arduino.h"
 #define scl PC6
 #define sda PC7
+uint8_t TM1650_CDigits[128] = {
+//0x00  0x01  0x02  0x03  0x04  0x05  0x06  0x07  0x08  0x09  0x0A  0x0B  0x0C  0x0D  0x0E  0x0F
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // 0x00
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // 0x10
+  0x00, 0x82, 0x21, 0x00, 0x00, 0x00, 0x00, 0x02, 0x39, 0x0F, 0x00, 0x00, 0x00, 0x40, 0x80, 0x00, // 0x20
+  0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7f, 0x6f, 0x00, 0x00, 0x00, 0x48, 0x00, 0x53, // 0x30
+  0x00, 0x77, 0x7C, 0x39, 0x5E, 0x79, 0x71, 0x6F, 0x76, 0x06, 0x1E, 0x00, 0x38, 0x00, 0x54, 0x3F, // 0x40
+  0x73, 0x67, 0x50, 0x6D, 0x78, 0x3E, 0x00, 0x00, 0x00, 0x6E, 0x00, 0x39, 0x00, 0x0F, 0x00, 0x08, // 0x50 
+  0x63, 0x5F, 0x7C, 0x58, 0x5E, 0x7B, 0x71, 0x6F, 0x74, 0x02, 0x1E, 0x00, 0x06, 0x00, 0x54, 0x5C, // 0x60
+  0x73, 0x67, 0x50, 0x6D, 0x78, 0x1C, 0x00, 0x00, 0x00, 0x6E, 0x00, 0x39, 0x30, 0x0F, 0x00, 0x00  // 0x70
+};
+void iicstart() {
+  digitalWrite(scl, 1);
+  delayMicroseconds(2);
+  digitalWrite(sda, 1);
+  delayMicroseconds(2);
+  digitalWrite(sda, 0);
+  delayMicroseconds(2);
+  digitalWrite(scl, 0);
+  delayMicroseconds(2);
+}
+void iicstop() {
+  digitalWrite(scl, 0);
+  delayMicroseconds(2);
+  digitalWrite(sda, 0);
+  delayMicroseconds(2);
+  digitalWrite(scl, 1);
+  delayMicroseconds(2);
+  digitalWrite(sda, 1);
+  delayMicroseconds(2);
+}
+void iicack() {
+  int v = digitalRead(sda);
+  while (v == 1) {
+    delay(10);
+    Serial_println_s("wite the call back");
+    v = digitalRead(sda);
+  }
+  Serial_println_s("Got the call back");
+  pinMode(sda, OUTPUT);
+  digitalWrite(scl, 0);
+  delayMicroseconds(2);
+  digitalWrite(sda, 1);
+  delayMicroseconds(2);
+  digitalWrite(scl, 1);
+  delayMicroseconds(2);
+  digitalWrite(scl, 0);
+  delayMicroseconds(2);
+}
+void iicWrite(uint8_t data) {
+  pinMode(sda, OUTPUT);
+  int t = data;
+  int arr[8];
+  for (int i = 0; i <= 7; i++) {
+    if (data % 2) {
+      arr[i] = 1;
+    }
+    else
+    {
+      arr[i] = 0;
+    }
+    data /= 2;
+  }
+  //if(dot){arr[0] = 1}else{arr[0] = 0}
+  for ( int j = 7; j >= 0; j--) {
+    if (arr[j])
+    {
+      digitalWrite(sda, 1);
+      delayMicroseconds(2);
+      digitalWrite(scl, 0);
+      delayMicroseconds(2);
+      digitalWrite(scl, 1);
+      delayMicroseconds(2);
+      digitalWrite(scl, 0);
+      delayMicroseconds(2);
+      digitalWrite(sda, 0);
+    } else {
+      delayMicroseconds(2);
+      digitalWrite(sda, 0);
+      delayMicroseconds(2);
+      digitalWrite(scl, 0);
+      delayMicroseconds(2);
+      digitalWrite(scl, 1);
+      delayMicroseconds(2);
+      digitalWrite(scl, 0);
+      delayMicroseconds(2);
+      digitalWrite(sda, 0);
+    }
+    pinMode(sda, OUTPUT);
+  }
+}
+void displayee(uint8_t add,uint8_t com){
+	iicstart();
+  iicWrite(0x48);
+  iicack();
+  iicWrite(0x11);
+  iicack();
+  iicstop();
+  iicstart();
+  iicWrite(add);
+  iicack();
+  iicWrite(com);
+  iicack();
+  iicstop();
+  //delay(20);
+}
