6f0c0c17125783dddf7f6ceabbc89ccf519232cb
yu123an <43343181+yu123an@users.noreply.github.com>
2019-11-17 15:59:49 +0800

    Update ds3231_arduino_wire.c

--- ds3231_arduino_wire.c
+++ ds3231_arduino_wire.c
@@ -4,6 +4,15 @@ void setup()
 {
   Wire_begin();
   Serial_begin(9600);
+  /*
+   * set the time
+  Wire_beginTransmission(0x68);
+  Wire_write(0x05);
+  int vall = 11;
+  int d = (vall/10*16) + (vall%10);
+  Wire_write(d);
+  Wire_endTransmission();
+  */
 }
 
 void loop()
@@ -11,10 +20,26 @@ void loop()
   Wire_beginTransmission(0x68);
   Wire_write(0x00);
   Wire_endTransmission();
-  Wire_requestFrom(0x68, 1);
+  Wire_requestFrom(0x68, 6);
 Serial_println_s("+--------------------+");
-    char val = Wire_read();
-    int c = (val/16*10) + (val%16) ;
-     Serial_println_i(c);
-  delay(500);
+char ss = Wire_read();
+char mm = Wire_read();
+char hh = Wire_read();
+char zz = Wire_read();
+char dd = Wire_read();
+char mo = Wire_read();
+int mouth = (mo/16*10) + (mo%16);
+int data = (dd/16*10) + (dd%16);
+int hour = (hh/16*10) + (hh%16);
+int minute = (mm/16*10) + (mm%16);
+Serial_print_s("now is:");
+Serial_print_i(mouth);
+Serial_print_s("-");
+Serial_print_i(data);
+Serial_print_s(":::");
+Serial_print_i(hour);
+Serial_print_s(":");
+Serial_print_i(minute);
+Serial_println_s("---");
+  delay(50);
 }
