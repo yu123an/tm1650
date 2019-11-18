8f873a4f78186e6547aac0014781a916c8a31a54
yu123an <43343181+yu123an@users.noreply.github.com>
2019-11-18 21:14:26 +0800

    add iwdg

--- stm8s_ds3231_tm1650.c
+++ stm8s_ds3231_tm1650.c
@@ -1,18 +1,26 @@
 #include <Wire.h>
+#include <stm8s_iwdg.h>
 uint8_t num[20] = {
   //0x00  0x01  0x02  0x03  0x04  0x05  0x06  0x07  0x08  0x09
   0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7f, 0x6f,
-  0xbf, 0x86, 0xdb, 0xcf, 0xe6, 0xed, 0xfd, 0x87, 0xff, 0xdf
+  0xbf, 0x86, 0xdb, 0xcf, 0xe6, 0xed, 0xfd, 0x87, 0xff, 0xef
 };
 void setup()
 {
+  /*
+     start the watch dog
+  */
+  IWDG_WriteAccessCmd(IWDG_WriteAccess_Enable);//允许操作独立看门狗
+  IWDG_SetPrescaler(IWDG_Prescaler_128);//看门狗预分频
+  IWDG_SetReload(255);//设置看门狗复位时间
+  IWDG_Enable();//启动独立看门狗
   Wire_begin();
   Serial_begin(9600);
   /*
-     set the time
+    // set the time
     Wire_beginTransmission(0x68);
-    Wire_write(0x05);
-    int vall = 11;
+    Wire_write(0x01);
+    int vall = 9;
     int d = (vall/10*16) + (vall%10);
     Wire_write(d);
     Wire_endTransmission();
@@ -37,30 +45,51 @@ void loop()
   int data1 = (dd / 16);
   int data2 = (dd % 16);
   int hour1 = (hh / 16);
-  int hour2 = (hh % 16 + 10);
-  int mi = (mm / 16);
+  int hour2 = (hh % 16 );
+  int mi = (mm / 16 );
   int minute2 = (mm % 16);
   //Serial_println_i(mouth1);
   /*
      display
   */
-  
-  Wire_beginTransmission(0x24);
-  Wire_write(0x11);
-  Wire_endTransmission();
-  Wire_beginTransmission(0x34);
-  Wire_write(num[hour1]);
-  Wire_endTransmission();
-  Wire_beginTransmission(0x35);
-  Wire_write(num[hour2]);
-  Wire_endTransmission();
-  Wire_beginTransmission(0x36);
-  Wire_write(num[mi]);
-  Wire_endTransmission();
-  Wire_beginTransmission(0x37);
-  Wire_write(num[minute2]);
-  Wire_endTransmission();
-  delay(1500);
+  for ( int a = 0; a < 9; a++) {
+    Wire_beginTransmission(0x24);
+    Wire_write(0x11);
+    Wire_endTransmission();
+    Wire_beginTransmission(0x34);
+    Wire_write(num[hour1]);
+    Wire_endTransmission();
+    Wire_beginTransmission(0x35);
+    Wire_write(num[hour2]);
+    Wire_endTransmission();
+    Wire_beginTransmission(0x36);
+    Wire_write(num[mi]);
+    Wire_endTransmission();
+    Wire_beginTransmission(0x37);
+    Wire_write(num[minute2]);
+    Wire_endTransmission();
+    for (int j = 0;j<=50;j++){
+    IWDG_ReloadCounter();
+    delay(10);}
+    Wire_beginTransmission(0x24);
+    Wire_write(0x11);
+    Wire_endTransmission();
+    Wire_beginTransmission(0x34);
+    Wire_write(num[hour1]);
+    Wire_endTransmission();
+    Wire_beginTransmission(0x35);
+    Wire_write(num[hour2]);
+    Wire_endTransmission();
+    Wire_beginTransmission(0x36);
+    Wire_write(num[mi + 10]);
+    Wire_endTransmission();
+    Wire_beginTransmission(0x37);
+    Wire_write(num[minute2 + 10]);
+    Wire_endTransmission();
+    for (int j = 0;j<=50;j++){
+    IWDG_ReloadCounter();
+    delay(10);}
+  }
   Wire_beginTransmission(0x24);
   Wire_write(0x11);
   Wire_endTransmission();
@@ -76,5 +105,7 @@ void loop()
   Wire_beginTransmission(0x37);
   Wire_write(num[data2]);
   Wire_endTransmission();
-  delay(1500);
+ for (int j = 0;j<=50;j++){
+    IWDG_ReloadCounter();
+    delay(10);}
 }
