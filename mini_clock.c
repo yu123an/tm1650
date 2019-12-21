59c79fbeb5dbf51ff6f2c5f42276c83fed9a8813
yu123an <43343181+yu123an@users.noreply.github.com>
2019-12-22 02:37:06 +0800

    增加温度显示

--- mini_clock.c
+++ mini_clock.c
@@ -1,7 +1,7 @@
 /*
-Stm8s
-tm1640
-ds3231
+  Stm8s
+  tm1640
+  ds3231
 */
 #include <Wire.h>
 int scl = 1;    //PA1
@@ -71,108 +71,158 @@ uint8_t num[20] = {
   0xbf, 0x86, 0xdb, 0xcf, 0xe6, 0xed, 0xfd, 0x87, 0xff, 0xef
 };
 uint8_t comm[8] = {
-  0x8f,0x8e,0x8d,0x8c,0x8b,0x8a,0x89,0x88
+  0x8f, 0x8e, 0x8d, 0x8c, 0x8b, 0x8a, 0x89, 0x88
 };
 void setup() {
   pinMode(scl, OUTPUT);
   pinMode(sda, OUTPUT);
   Wire_begin();
-  //Serial_begin(9600);
-  /* Wire_beginTransmission(0x68);
-    Wire_write(0x00);
-    Wire_write(bin2bcd(46));
-    
-    Wire_endTransmission();
-    Wire_beginTransmission(0x68);
-    Wire_write(0x01);
-    Wire_write(bin2bcd(2));
-     Wire_endTransmission();
-    
-    Wire_beginTransmission(0x68);
-    Wire_write(0x02);
-    Wire_write(bin2bcd(22));
-     Wire_endTransmission();
-    Wire_beginTransmission(0x68);
-    Wire_write(0x04);
-    Wire_write(bin2bcd(20));
-     Wire_endTransmission();
-    Wire_beginTransmission(0x68);
-    Wire_write(0x05);
-    Wire_write(bin2bcd(12));
-     Wire_endTransmission();
-    Wire_beginTransmission(0x68);
-    Wire_write(0x06);
-    Wire_write(bin2bcd(19));
-    Wire_endTransmission();
-    */
+  Serial_begin(9600);
+  /*
+  Wire_beginTransmission(0x68);
+  Wire_write(0x00);
+  Wire_write(bin2bcd(46));
+
+  Wire_endTransmission();
+  Wire_beginTransmission(0x68);
+  Wire_write(0x01);
+  Wire_write(bin2bcd(2));
+  Wire_endTransmission();
+
+  Wire_beginTransmission(0x68);
+  Wire_write(0x02);
+  Wire_write(bin2bcd(22));
+  Wire_endTransmission();
+  
+  Wire_beginTransmission(0x68);
+  Wire_write(0x04);
+  Wire_write(bin2bcd(20));
+  Wire_endTransmission();
+  
+  Wire_beginTransmission(0x68);
+  Wire_write(0x05);
+  Wire_write(bin2bcd(12));
+  Wire_endTransmission();
+  
+  Wire_beginTransmission(0x68);
+  Wire_write(0x06);
+  Wire_write(bin2bcd(19));
+  Wire_endTransmission();
+*/
 }
 void loop() {
   int light = (analogRead(led) / 4);
-//  Serial_println_i(analogRead(led));
-  if(light > 7){
+  //  Serial_println_i(analogRead(led));
+  if (light > 7) {
     light = 7;
   }
- // Serial_println_i(light);
-  //get the time
   Wire_beginTransmission(0x68);
   Wire_write(0x00);
   Wire_endTransmission();
-  Wire_requestFrom(0x68, 6);
-  char ss = Wire_read();
-  char mm = Wire_read();
-  char hh = Wire_read();
- // char zz = Wire_read();
-//  char dd = Wire_read();
-//  char mo = Wire_read();
+  Wire_requestFrom(0x68, 19);
+  int ss = Wire_read();
+  int mm = Wire_read();
+  int hh = Wire_read();
+  int zz = Wire_read();
+  int dd = Wire_read();
+  int mo = Wire_read();
+  Wire_read();
+  Wire_read();
+  Wire_read();
+  Wire_read();
+  Wire_read();
+  Wire_read();
+  Wire_read();
+  Wire_read();
+  Wire_read();
+  Wire_read();
+  Wire_read();
+  int tmp = Wire_read();
+  int tmp1 = tmp / 16;
+  int tmp2 = tmp % 16;
   int ss1 = (ss / 16);
   int ss2 = (ss % 16);
- // int day = (hh % 16);
- // int mouth1 = (mo / 16);
-//  int mouth2 = (mo % 16);
- // int data1 = (dd / 16);
-//  int data2 = (dd % 16);
+  int mouth1 = (mo / 16);
+  int mouth2 = (mo % 16);
+  int data1 = (dd / 16);
+  int data2 = (dd % 16);
   int hour1 = (hh / 16);
   int hour2 = (hh % 16 );
-//  int sss = ((ss1 * 10) + ss2);
+  int sss = ((ss1 * 10) + ss2);
   int minute1 = (mm / 16 );
   int minute2 = (mm % 16);
-//  Serial_println_i(sss);
- if ((hour % 3) == 0){
-    if(minute == 23){
-       Wire_endTransmission();
-    Wire_beginTransmission(0x68);
-    Wire_write(0x01);
-    Wire_write(bin2bcd(24));
-     Wire_endTransmission();
+  int hour = ((hour1 * 10) + hour2);
+  int minute = ((minute1 * 10) + minute2);
+  if ((hour % 3) == 0) {
+    if (minute == 23) {
+      Wire_endTransmission();
+      Wire_beginTransmission(0x68);
+      Wire_write(0x01);
+      Wire_write(bin2bcd(24));
+      Wire_endTransmission();
     }
   }
+  // Serial_println_i(sss);
+  //Serial_println_i(tmp1 * 10 + tmp2);
+  i2c_start();
+  i2c_Write(0x40);
+  i2c_stop();
+  i2c_start();
+  i2c_Write(0xc0);
+  i2c_Write(num[hour1]);
+  i2c_Write(num[hour2]);
+  i2c_Write(num[minute1]);
+  i2c_Write(num[minute2]);
+  i2c_stop();
+  i2c_start();
+  i2c_Write(comm[light]);
+  i2c_stop();
+  delay(500);
+  i2c_start();
+  i2c_Write(0x40);
+  i2c_stop();
+  i2c_start();
+  i2c_Write(0xc0);
+  i2c_Write(num[hour1]);
+  i2c_Write(num[hour2 + 10]);
+  i2c_Write(num[minute1 + 10]);
+  i2c_Write(num[minute2]);
+  i2c_stop();
+  i2c_start();
+  i2c_Write(comm[light]);
+  i2c_stop();
+  delay(480);
+  if (sss == 58) {
+    for (int ii = 0; ii <= 19; ii++) {
+      i2c_start();
+      i2c_Write(0x40);
+      i2c_stop();
+      i2c_start();
+      i2c_Write(0xc0);
+      for (int i = 0; i <= 3; i++) {
+        int a = random_minmax(0, 19);
+        i2c_Write(num[a]);
+      }
+      i2c_stop();
+      i2c_start();
+      i2c_Write(comm[light]);
+      i2c_stop();
+      delay(98);
+    }
+  } else if ((sss % 10) == 0) {
     i2c_start();
     i2c_Write(0x40);
     i2c_stop();
     i2c_start();
     i2c_Write(0xc0);
-    i2c_Write(num[hour1]);
-    i2c_Write(num[hour2]);
-    i2c_Write(num[minute1]);
-    i2c_Write(num[minute2]);
+    i2c_Write(num[tmp1]);
+    i2c_Write(num[tmp2]);
+    i2c_Write(B11100001);
+    i2c_Write(0);
     i2c_stop();
     i2c_start();
     i2c_Write(comm[light]);
     i2c_stop();
-    delay(500);
-     i2c_start();
-    i2c_Write(0x40);
-    i2c_stop();
-    i2c_start();
-    i2c_Write(0xc0);
-    i2c_Write(num[hour1]);
-    i2c_Write(num[hour2 + 10]);
-    i2c_Write(num[minute1 + 10]);
-    i2c_Write(num[minute2]);
-    i2c_stop();
-    i2c_start();
-    i2c_Write(comm[light]);
-    i2c_stop();
-    delay(480);
-   
+    delay(3000);
+  }
 }
